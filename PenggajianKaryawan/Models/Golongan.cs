0673ac6c82ebfe225433eb2364f12e04077acb27
anshoria <anshoriakbar11@gmail.com>
2021-11-23 23:49:51 +0700

    Add project files.

---
+++ PenggajianKaryawan/Models/Golongan.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace PenggajianKaryawan.Models
+{
+    public partial class Golongan
+    {
+        public int? IdGologan { get; set; }
+        public string Nama { get; set; }
+        public string JenisGolongan { get; set; }
+    }
+}
